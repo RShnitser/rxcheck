0473b25582969857185484988fc242f5a5da53c7
RShnitser <romanshnitser@hotmail.com>
2025-02-04 23:03:13 -0500

    add user to database

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ toolchain go1.23.5
 
 require (
 	github.com/a-h/templ v0.3.819 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
 )
--- users.go
+++ users.go
@@ -3,6 +3,9 @@ package main
 import(
 	"net/http"
 	"fmt"
+
+	"rxcheck/internal/auth"
+	"rxcheck/internal/database"
 )
 
 func(cfg *config)handleAddUser(w http.ResponseWriter, r *http.Request){
@@ -13,4 +16,16 @@ func(cfg *config)handleAddUser(w http.ResponseWriter, r *http.Request){
 	password := r.FormValue("password")
 
 	fmt.Printf("name: %s, pass: %s\n", userName, password)
+
+	hash, err := auth.HashPassword(password)
+	if err != nil {
+		//respondWithError(w, http.StatusInternalServerError, "Couldn't hash password", err)
+		return
+	}
+
+	_, err = cfg.db.CreateUser(r.Context(), database.CreateUserParams{userName, hash})
+	if err != nil {
+		//respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
+		return
+	}
 }
