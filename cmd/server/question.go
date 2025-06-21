b470fc64d7a3a53d76517c06da627c669728e71d
RShnitser <romanshnitser@hotmail.com>
2025-06-21 16:28:43 -0400

    complete question endpoint

--- cmd/server/question.go
+++ cmd/server/question.go
@@ -3,9 +3,10 @@ package main
 import(
 	"net/http"
 	"rxcheck/internal/auth"
-	"rxcheck/internal/database"
+	// "rxcheck/internal/database"
 	"rxcheck/templates"
 	"strconv"
+	"github.com/google/uuid"
 )
 
 func (cfg *config)handleGetQuestion(w http.ResponseWriter, r *http.Request){
@@ -23,4 +24,35 @@ func (cfg *config)handleGetQuestion(w http.ResponseWriter, r *http.Request){
 	if err != nil{
 		return
 	}
+
+	quiz, err := cfg.db.GetQuizByUserID(r.Context(), userID)
+	if err != nil{
+		return
+	}
+
+	var questionID uuid.UUID
+	switch nextQuestionIndex{
+	case 0:
+		questionID = quiz.Question1
+	case 1:
+		questionID = quiz.Question2
+	case 2:
+		questionID = quiz.Question3
+	case 3:
+		questionID = quiz.Question4
+	case 5:
+		questionID = quiz.Question5
+	default:
+		return
+	}
+
+	question, err := cfg.db.GetQuestionByID(r.Context(), questionID)
+	if err != nil{
+		return
+	}
+
+	if nextQuestionIndex >= 4{
+		return
+	}
+	templates.Question(question, int32(nextQuestionIndex + 1)).Render(r.Context(), w)
 }
